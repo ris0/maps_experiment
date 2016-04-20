e5b1c9aa2cf2cff922b521c3ec1bd5d809e87865
Rich Song <richasong@gmail.com>
2016-04-20 01:03:10 -0400

    Initial layout

---
+++ server/start.js
@@ -0,0 +1,12 @@
+/*
+
+ ES6 by default! Using http://babeljs.io
+
+ Our actual application code begins inside of
+ server/main.js, but the process should be started
+ from this file in order to enable ES6 translation.
+
+*/
+
+require('babel/register');
+require('./main');
